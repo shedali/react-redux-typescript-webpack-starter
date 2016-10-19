121927037a918d3069f3a845af7c3ae7750415a4
reanimatter <reanimatter@gmail.com>
2016-10-18 20:36:08 -0700

    invalid import bug fix

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,1 +1,4 @@
-export * from 'lib/components/HelloWorld';
+export * from 'lib/components/hello-world';
+export * from 'lib/components/nav';
+export * from 'lib/components/nav-item';
+export * from 'lib/components/navbar';
